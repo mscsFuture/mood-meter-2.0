267a6d09ffbdcd046bc69ad2fbf64552bd193c4a
Maverick Berner <23459899+Mavybeans@users.noreply.github.com>
2024-06-06 16:05:17 -0500

    Co-authored-by: BenFen25 <BenFen25@users.noreply.github.com>

---
+++ login-script.js
@@ -0,0 +1,33 @@
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	console.log("Login Button Clicked")
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+window.onload = function() {
+	console.log("Hellow World");
+}
